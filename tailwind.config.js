e797ac7e4f164c76d105e99e890c9a85096c7441
Christian Hannachi <hannachichristian02@gmail.com>
2025-01-09 11:23:36 +0100

    Test 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        shimmer: {
+          '0%': { backgroundPosition: '200% center' },
+          '100%': { backgroundPosition: '-200% center' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 1s linear infinite',
+      },
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
       },
